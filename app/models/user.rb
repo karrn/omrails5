871faef934b5cd8213ee525219bad944b077adef
Karan <karan.ratanpal@gmail.com>
2016-11-05 02:50:54 -0700

    Added Username and Names to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :tweets
   has_many :recipes
   has_many :items
+
+
+  validates :username, presence: true, uniqueness: true, length: {minimum: 6}
+  validates :name, presence: true
 end
