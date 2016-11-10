c866cf9ec59c4bad4b57c986e6b27baee2cea536
Karan <karan.ratanpal@gmail.com>
2016-11-09 18:25:06 -0800

    Modified recipe model due to heroku paperclip errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,6 @@ class Recipe < ApplicationRecord
   validates :directions, presence: true
 
 
-  has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>" }
+  has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
